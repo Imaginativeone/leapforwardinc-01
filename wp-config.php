2074981d45917ebceb70d559a7b22401566aa9f5
Doug Franklin <doug@dougfranklin.com>
2020-08-25 22:14:55 -0400

    WP Config File Updated

--- wp-config.php
+++ wp-config.php
@@ -19,23 +19,47 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
+if (strstr($_SERVER['SERVER_NAME'], 'leapforward01.local')) {
 
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+	/** The name of the database for WordPress */
+	define( 'DB_NAME', 'local' );
 
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+	/** MySQL database username */
+	define( 'DB_USER', 'root' );
 
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+	/** MySQL database password */
+	define( 'DB_PASSWORD', 'root' );
 
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+	/** MySQL hostname */
+	define( 'DB_HOST', 'localhost' );
+
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+
+} else {
+
+	define( 'DB_NAME', 'imagin42_wp582' );
+
+	/** MySQL database username */
+	define( 'DB_USER', 'imagin42_wp582' );
+
+	/** MySQL database password */
+	define( 'DB_PASSWORD', ']Sp93c74v[' );
+
+	/** MySQL hostname */
+	define( 'DB_HOST', 'localhost' );
+
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8mb4' );
+
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+
+}
 
 /**
  * Authentication Unique Keys and Salts.
