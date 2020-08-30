6e106b0db5414ec697757a318f10d0a807a4213f
Doug Franklin <doug@dougfranklin.com>
2020-08-30 10:08:39 -0400

    Video 007 Basic Functions

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,18 @@
-This is our completely amazing custom theme.
+<?php 
+
+  function myFirstFunction($parameter, $secondParameter) {
+    echo $parameter, ' The answer is: ', $secondParameter*$secondParameter;
+  }
+
+  function greet($name, $favoriteColor) {
+    echo "<p>Hi, my name is $name and my favorite color is $favoriteColor.</p>";
+  }
+
+  $argument = 'Hello';
+  $secondArgument = 7;
+
+  myFirstFunction($argument, $secondArgument);
+
+  greet('John', 'Blue');
+  greet('Jane', 'Green');
+?>
