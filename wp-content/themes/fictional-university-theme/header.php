4627945a118bd6e3c528dd5688ca5f05254bfc48
Doug Franklin <doug@dougfranklin.com>
2020-08-31 08:46:41 -0400

    S4V010 Loading style.css from functions.php

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,1 +1,10 @@
-Greetings from header.php
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- See functions.php -->
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <h1>Fictional University</h1>
+  </body>
+</html>
