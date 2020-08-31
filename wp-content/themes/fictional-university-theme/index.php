d7ded8b118eb25e9a0e623e7a79c8e02f623a234
Doug Franklin <doug@dougfranklin.com>
2020-08-31 15:07:12 -0400

    S4V011 library-hero.jpg added via get_theme_file_uri()

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -4,7 +4,8 @@
 
   <!-- // Begin HTML Mode -->
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg);"></div>
+    <!-- Use WordPress to locate the image -->
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome!</h1>
       <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
