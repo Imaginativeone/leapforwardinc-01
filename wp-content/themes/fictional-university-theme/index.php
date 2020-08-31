9f2d2b810005acce6b0bed98c6f46f9ad80c45f6
Doug Franklin <doug@dougfranklin.com>
2020-08-30 20:27:38 -0400

    S4V009 Loop Updates

---
+++ wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,12 @@
+<?php 
+  while(have_posts()) { // Famous WordPress Loop!
+    the_post(); ?>
+
+    <!-- Still within the while loop, but in HTML Mode -->
+    <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+    <div><?php the_content()?></div>
+    <hr>
+    <!-- Exit HTML Mode -->
+
+  <?php }
+?>
