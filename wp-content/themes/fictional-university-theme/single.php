7ea3ca3854e551df0b0171da6de35f70f48b53ee
Doug Franklin <doug@dougfranklin.com>
2020-08-30 20:44:53 -0400

    Custom Single-Post Page - Updated

--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -1,1 +1,12 @@
-Hello 123
+<?php 
+  while(have_posts()) { // Famous WordPress Loop!
+    the_post(); ?>
+
+    <!-- Still within the while loop, but in HTML Mode -->
+    <!-- Headline Links Removed -->
+    <h2><?php the_title()?></h2>
+    <div><?php the_content()?></div>
+    <!-- Exit HTML Mode -->
+
+  <?php }
+?>
