ba4bab23880355ab1ba170242a12be177509f782
Doug Franklin <doug@dougfranklin.com>
2020-09-09 13:36:59 -0400

    S5V014 I am a parent/child page

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -23,13 +23,16 @@
 
       <!-- If the current page has a parent page -->
       <?php
-        if (wp_get_post_parent_ID(get_the_ID())) {
+
+        $theParent = wp_get_post_parent_ID(get_the_ID());
+
+        if ($theParent) {
           echo "I am a child page."; ?>
           <!-- Breadcrumb Box -->
           <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-              <a class="metabox__blog-home-link" href="#">
-                <i class="fa fa-home" aria-hidden="true"></i> Back to About Us
+              <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?><!-- pass in an id -->
               </a>
               <!-- See functions.php -->
               <span class="metabox__main"><?php the_title(); ?></span>
