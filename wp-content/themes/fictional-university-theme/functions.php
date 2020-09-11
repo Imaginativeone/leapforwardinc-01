9d2b44ba830bc5fe11e2a7f135ede9e5e4d24a8f
Doug Franklin <doug@dougfranklin.com>
2020-09-11 19:01:11 -0400

    Vendors Added

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -9,7 +9,8 @@
       wp_enqueue_style('university_main_styles', get_stylesheet_uri());
       // wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
-      // wp_enqueue_script('our-vendors-js',     get_theme_file_uri('/bundled-assets/vendors.js'), NULL, '1.0', true);
+
+      wp_enqueue_script('our-vendors-js',     get_theme_file_uri('/bundled-assets/vendors.js'), NULL, '1.0', true);
       // wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.js'), NULL, '1.0', true);
       // wp_enqueue_style ('our-main-styles',    get_theme_file_uri('/bundled-assets/styles.css'));
       
