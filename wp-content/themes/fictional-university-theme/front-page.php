9a8e0b01b24e299535547c229a85a43436301f42
Doug Franklin <doug@dougfranklin.com>
2020-09-11 16:46:14 -0400

    S6V2324 Custom Queries for Front Page Dynamic Posts

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -45,9 +45,43 @@
     </div>
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
-        <div class="event-summary">
+        <!-- Custom Query -->
+        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <!-- Hello World. Testing 123 -->
+        <?php 
+
+          // Custom Query
+          $homepagePosts = new WP_Query(
+            array(
+              'posts_per_page' => 2
+              // 'post_type' => 'post',
+              // 'category_name'  => 'awards'
+            )
+          );
+
+          while($homepagePosts->have_posts()) { // replaced have_posts()
+            $homepagePosts->the_post(); ?>
+            <!-- <li><?php the_title(); ?></li> --><!-- Home Static Page (no parameters) -->
+            <div class="event-summary">
+              <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month"><?php the_time('M'); ?></span>
+                <span class="event-summary__day"><?php the_time('d'); ?></span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny">
+                  <a href="<?php the_permalink(); ?>">
+                    <?php the_title() ?>
+                  </a>
+                </h5>
+                <p><?php echo wp_trim_words(get_the_content(), 18); ?>
+                  <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+              </div>
+            </div>
+          <?php } wp_reset_postdata();
+        ?>
+
+        <!-- <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="#">
             <span class="event-summary__month">Jan</span>
             <span class="event-summary__day">20</span>
@@ -57,6 +91,7 @@
             <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
           </div>
         </div>
+
         <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="#">
             <span class="event-summary__month">Feb</span>
@@ -66,9 +101,11 @@
             <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
             <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
           </div>
-        </div>
+        </div> -->
 
-        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        <!-- All Blog Posts -->
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
+
       </div>
     </div>
   </div>
