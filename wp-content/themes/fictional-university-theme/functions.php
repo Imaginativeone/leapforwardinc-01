a53dfa0b82f92adca93aaf0071a49c2b9a25c0d0
Doug Franklin <doug@dougfranklin.com>
2020-09-18 13:02:02 -0400

    Uploaded Images

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -35,8 +35,9 @@
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails'); // Thumbnails for Blog Posts, but not CPTs yet
                                           // See Must-Use Plugins > Professor Post Type > 'supports'->'thumbnail
-    add_image_size('professor-landscape', 400, 260, true); // false crop
-    add_image_size('professor-portrait',  480, 650, true); // false crop
+    add_image_size('professor-landscape', 400,  260, true); // false crop
+    add_image_size('professor-portrait',  480,  650, true); // false crop
+    add_image_size('pageBanner',         1500,  350, true); // false crop
   }
 
   add_action('after_setup_theme', 'university_features'); // Header Title
