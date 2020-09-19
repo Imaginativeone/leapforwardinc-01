6fdce03bb7d70992cea1df761c02b31eb5803745
Doug Franklin <doug@dougfranklin.com>
2020-09-19 07:02:01 -0400

    Note CPTs are presented

--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -17,6 +17,32 @@
 
     <div class="container container--narrow page-section">
       Custom Code will go here.
+      <ul class="min-list link-list" id="my-notes">
+        <!-- We want to create one list item for each note post. -->
+        <?php
+          // Ask the database for only the exact posts that we are interested in. 
+          $userNotes = new WP_Query(
+            array(
+              'post_type' => 'note',
+              'posts_per_page' => -1,
+              'author' => get_current_user_id() // Current User
+            )
+          );
+
+          while($userNotes->have_posts()) {
+            $userNotes->the_post(); ?>
+            
+            <!-- Beg HTML -->
+            <li>
+              <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+              <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+            </li>
+            <!-- End HTML -->
+
+            <?php
+          }
+        ?>
+      </ul>
     </div>
 
 
