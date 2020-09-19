196470434381fb9990f48a66d01e84398237c8d7
Doug Franklin <doug@dougfranklin.com>
2020-09-19 06:36:13 -0400

    New Custom Post Type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -90,6 +90,48 @@
       'menu_icon' => 'dashicons-calendar'
     ));
 
+    // 01. Note Post Type
+    register_post_type('note', array(           // 02. 'note'
+      'show_in_rest' => true,                   // 03. Modern Editor
+      // 'capability_type' => 'cpt-template',   // 04. 'event' permissions, 'post' by default, see next line
+      // 'map_meta_cap'    => true,             // 05. These two lines add 'Events' to the Members Plugin
+      'supports' => array('title', 'editor', 'excerpt'), // CPT Excerpts now happen, 'editor' is mandatory
+      // 'custom-fields' removed, using ACF now
+      // 'rewrite' => array('slug' => 'cpt-templates'), // Custom Archive URL
+      'has_archive' => true,
+      'public' => false,                        // Usually true
+      'show_ui' => true, 
+      'labels' => array(
+        'name' => 'Notes',                      // Menu Item
+        'add_new_item' => 'Add New Note',
+        'edit_item' => 'Edit Note',
+        'all_items' => 'All Notes',
+        'singular_name' => 'Note'
+      ),
+      'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
+
+    // 01. CPT Post Type (Template)
+    register_post_type('cpt', array(            // 02
+      'show_in_rest' => true,                   // 03 Modern Editor
+      // 'capability_type' => 'cpt',            // 04 'cpt' permissions, 'post' by default, see next line
+      // 'map_meta_cap'    => true,             // 05 These two lines add 'CPTs' to the Members Plugin
+      'supports' => array('title', 'editor', 'excerpt'), // CPT Excerpts now happen, 'editor' is mandatory
+      // 'custom-fields' removed, using ACF now
+      // 'rewrite' => array('slug' => 'cpts'), // Custom Archive URL
+      'has_archive' => true,
+      'public' => true,                        // Usually true
+      'show_ui' => true, 
+      'labels' => array(
+        'name' => 'CPTs',                      // Menu Item
+        'add_new_item' => 'Add New CPT',
+        'edit_item' => 'Edit CPT',
+        'all_items' => 'All CPTs',
+        'singular_name' => 'CPT'
+      ),
+      'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
+
   }
 
   add_action('init', 'university_post_types');
