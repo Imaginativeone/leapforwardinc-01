3de37a49b8ff6e78949b26ad2967a90517f523a8
Doug Franklin <doug@dougfranklin.com>
2020-09-20 06:34:41 -0400

    Basic Search Capability

--- wp-content/themes/fictional-university-theme/js/scripts.js
+++ wp-content/themes/fictional-university-theme/js/scripts.js
@@ -8,9 +8,9 @@ import Search     from "./modules/Search"
 import MyNotes    from "./modules/MyNotes"
 
 // Instantiate a new object using our modules/classes
-var mobileMenu  = new MobileMenu()
-var heroSlider  = new HeroSlider()
-var search      = new Search();
+var mobileMenu    = new MobileMenu()
+var heroSlider    = new HeroSlider()
+var search        = new Search();
 
 var myNotes = new MyNotes();
 
