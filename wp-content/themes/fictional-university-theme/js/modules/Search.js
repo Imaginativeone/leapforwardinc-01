d72171a2f574b0dff374aef0929f1793fb09043e
Doug Franklin <doug@dougfranklin.com>
2020-09-22 04:44:54 -0400

    Add info to searchField - only when it has the focus

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,12 +6,15 @@ class Search {
   constructor() {
     // alert('Hello, I am a Search.');
 
+    this.resultsDiv  = $("#search-overlay__results");
     this.openButton  = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
     this.typingTimer;
   }
 
@@ -22,7 +25,7 @@ class Search {
 
     // Keyboard Events
     $(document).on('keydown', this.keyPressDispatcher.bind(this));
-    this.searchField.on('keydown', this.typingLogic.bind(this)); // See constructor
+    this.searchField.on('keyup', this.typingLogic.bind(this)); // See constructor
   }
 
   // Locating Keycodes S13V58-08:00
@@ -30,20 +33,43 @@ class Search {
   // 3. Methods
   typingLogic() {
     // alert("Hello from typingLogic()");
-    clearTimeout(this.typingTimer);
-    this.typingTimer = setTimeout(() => {
-      console.log('This is a timeout test.');
-    }, 2000);
+
+    if (this.searchField.val() != this.previousValue) {
+
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.val()) {
+
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+    
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+  
+      } else {
+        this.resultsDiv.html('');
+        this.isSpinnerVisible = false;
+      }
+  
+    }
+
+    this.previousValue = this.searchField.val();
+  }
+
+  getResults() {
+    this.resultsDiv.html("Image real search results here.");
+    this.isSpinnerVisible = false;
   }
 
   keyPressDispatcher(e) { // The parameter contains the key, s:83, Esc: 27
     // console.log('This is a test.', e.keyCode);
 
-    if (e.keyCode == 83 && !this.isOverlayOpen) {
+    if (e.keyCode == 83 && !this.isOverlayOpen && $("input, textarea").is(':focus')) {
       this.openOverlay();
     }
 
-    if (e.keyCode == 27 && this.isOverlayOpen) {
+    if (e.keyCode == 27 &&  this.isOverlayOpen && $("input, textarea").is(':focus')) {
       this.closeOverlay();
     }
 
