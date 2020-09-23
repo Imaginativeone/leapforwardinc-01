0cb51f75a8ebedb984fff20a5caee4bb67aa51f1
Doug Franklin <doug@dougfranklin.com>
2020-09-23 18:31:00 -0400

    Overlay HTML Moved to JavaScript

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -13,8 +13,8 @@
     } else {
 
       wp_enqueue_script('our-vendors-js',     get_theme_file_uri('/bundled-assets/vendors~scripts.9678b4003190d41dd438.js'), NULL, '1.0', true);
-      wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.c4e95a7f92a10003703b.js'), NULL, '1.0', true);
-      wp_enqueue_style ('our-main-styles',    get_theme_file_uri('/bundled-assets/styles.c4e95a7f92a10003703b.css'));
+      wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.49bed9f12de449677a19.js'), NULL, '1.0', true);
+      wp_enqueue_style ('our-main-styles',    get_theme_file_uri('/bundled-assets/styles.49bed9f12de449677a19.css'));
       
       wp_enqueue_script('main-university-javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
       wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -194,7 +194,7 @@
   add_action('login_enqueue_scripts', 'ourLoginCSS');
 
   function ourLoginCSS() {
-    wp_enqueue_style ('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c4e95a7f92a10003703b.css'));
+    wp_enqueue_style ('our-main-styles', get_theme_file_uri('/bundled-assets/styles.49bed9f12de449677a19.css'));
   }
 
   function my_login_logo_url() {
--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,6 +6,7 @@ class Search {
   constructor() {
     // alert('Hello, I am a Search.');
 
+    this.addSearchHTML(); // This goes first, in order to make the other lines relevant.
     this.resultsDiv  = $("#search-overlay__results");
     this.openButton  = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -114,6 +115,25 @@ class Search {
 
     console.log('Our close method just ran.');
     this.isOverlayOpen = false;
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+    <!-- <div class="search-overlay search-overlay--active"> -->
+    <div class="search-overlay">
+      <div class="search-overlay__top">
+        <div class="container">
+          <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+          <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+          <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div class="container">
+        <div id="search-overlay__results"></div>
+      </div>
+
+    </div>
+    `);
   }
 
 }
