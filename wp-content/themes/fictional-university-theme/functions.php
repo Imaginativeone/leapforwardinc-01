30c9a24e53165071dac048cc89375e12677a61a9
Doug Franklin <doug@dougfranklin.com>
2020-10-01 08:39:09 -0400

    university-features Sent to includes

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -3,44 +3,9 @@
   require get_theme_file_path('/includes/search-route.php');
   require get_theme_file_path('/includes/inc-university-custom-rest.php');
   require get_theme_file_path('/includes/inc-university-files.php');
+  require get_theme_file_path('/includes/inc-university-features.php'); // Current Development; Done
 
-  function university_features() {
-    
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocation1', 'Footer Location One');
-    register_nav_menu('footerLocation2', 'Footer Location Two');
-
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails'); // Thumbnails for Blog Posts, but not CPTs yet
-                                          // See Must-Use Plugins > Professor Post Type > 'supports'->'thumbnail
-    add_image_size('professor-landscape', 400,  260, true); // false crop
-    add_image_size('professor-portrait',  480,  650, true); // false crop
-    add_image_size('pageBanner',         1500,  350, true); // false crop
-  }
-
-  add_action('after_setup_theme', 'university_features'); // Header Title
-
-  // See archive-event.php for info about Default Queries Adjustments
-  // $today = date('Ymd');
-
-  // Custom Query with Ordering and Sorting
-  // $homepageEvents = new WP_Query(
-  //   array(
-  //     'posts_per_page' => -1, // 2
-  //     'post_type'      => 'event',
-  //     'meta_key'       => 'event_date',
-  //     'orderby'        => 'meta_value_num', // formerly 'post_date', 'rand', meta_value !event_date
-  //     'order'          => 'ASC',
-  //     'meta_query'     => array( // eliminate non-adherents to sub-query
-  //       array(
-  //         'key' => 'event_date',
-  //         'compare' => '>=',
-  //         'value' => $today,
-  //         'type' => 'numeric'
-  //       )
-  //     )
-  //   )
-  // );
+  // Hello
 
   function university_adjust_queries($query) {
 
